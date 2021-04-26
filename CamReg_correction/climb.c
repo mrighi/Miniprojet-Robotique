6845baf89d102edffc69d1a9a509bd128b84bf4a
Matteo Righi <matteo.righibassu@gmail.com>
2021-04-26 22:13:35 +0200

    Updated climbing algorithm

--- CamReg_correction/climb.c
+++ CamReg_correction/climb.c
@@ -16,18 +16,41 @@
 #include <motors.h>
 #include <climb.h>
 
-int16_t imu_angle(){ //Very uncertain about the variable type
-
-}
-
-int16_t prox_angle(){
-	if(get_acc_filtered(Z_AXIS)-offset_z >= 1 - PLATEAU_DETECTION_THRESHOLD){
+double imu_angle(){ //That we have doubles is most certainly not good
+	if(get_acc_filtered(Z_AXIS, IMU_SAMPLE_SIZE)-offset_z >= 1 - PLATEAU_DETECTION_THRESHOLD){
 		return 0;  //Maybe blink a led or smth ?
 	}
+	elseif(get_acc_filtered(Y_AXIS, IMU_SAMPLE_SIZE)-offset_y == 0 && get_acc_filtered(X_AXIS, IMU_SAMPLE_SIZE)-offset_x > 0){ //arctan gives pi/2 case
+		return 1.57 ; //pi/2
+	}
+	elseif(get_acc_filtered(Y_AXIS, IMU_SAMPLE_SIZE)-offset_y == 0 && get_acc_filtered(X_AXIS, IMU_SAMPLE_SIZE)-offset_x < 0){
+			return -1.57 ; //pi/2
+		}
 	else{
-
+		return atan((get_acc_filtered(X_AXIS, IMU_SAMPLE_SIZE)-offset_x)/(get_acc_filtered(Y_AXIS, IMU_SAMPLE_SIZE)-offset_y));
 	}
 }
+
+int16_t prox_angle(){ //Very uncertain about the variable type
+	//Shoudln't give an exact angle, so much as a weight to the motors:
+	weight_left=1;
+	weight_left=1;
+	if(get_calibrated_prox(FRONT_RIGHT)<PROX_THRESHOLD){
+		weight_left+=5
+	}
+	elseif(get_calibrated_prox(FRONT_LEFT)<PROX_THRESHOLD){
+		weight_right+=5
+	}
+	if(get_calibrated_prox(FRONT__DIAG_RIGHT)<PROX_THRESHOLD){ //not elseif because if the obstacle stretches further, turn more
+		weight_left+=3
+	}
+	elseif(get_calibrated_prox(FRONT__DIAG_LEFT)<PROX_THRESHOLD){
+		weight_right+=3
+	}
+	return ??? //I forgot the syntax for this
+}
+//should give five different cases, depending on which sensors are tripped, and corresponding
+//predetermined angles for each case
 
 static THD_WORKING_AREA(waSetPath, 256); //Should not need much memory (no tables)
 
@@ -57,6 +80,7 @@ static THD_FUNCTION(SetPath, arg) {
     	time = chVTGetSystemTime();
 
     	//Speed is a weighted average of prox and ir values
+    	//Penalty optmisation problem
 
     	chThdSleepUntilWindowed(time, time + MS2ST(10)); //100 Hz
     }
