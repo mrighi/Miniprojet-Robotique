f2538f9302a1bf3f3adf4e9bf73cef14bf74bfb6
Matteo Righi <matteo.righibassu@gmail.com>
2021-04-29 14:38:00 +0200

    Implemented conditional climb algorithm

--- CamReg_correction/climb.c
+++ CamReg_correction/climb.c
@@ -1,11 +1,10 @@
-/*
- * QUESTIONS:
- *  - Use get_acc_filtered ?
- *  - Am I using offsets correctly ?
- *  - Angles are in float, can we get in int ?
- */
 //MUST CORRECT:
-//PROBABLY SWITCH ACC_X AND ACC_X_CALIBRATED
+//MAX_SPEED is already in motors.h
+//Use filtered accelerometer values
+//See if it's possible to optimize the giant if tree
+//Remove if cases where the motor doesn't need to be updated
+//In this form, it's redundant to use atan because we only turn left or right
+
 #include "ch.h"
 #include "hal.h"
 #include <math.h>
@@ -21,139 +20,302 @@
 
 static BSEMAPHORE_DECL(sendToComputer_sem, TRUE);
 
+void turn_left(void){
+	left_motor_set_speed(SPEED_MAX);
+	right_motor_set_speed(-SPEED_MAX);
+}
+
+void turn_right(void){
+	left_motor_set_speed(SPEED_MAX);
+	right_motor_set_speed(-SPEED_MAX);
+}
+
+void stop_motors(void){
+	left_motor_set_speed(0);
+	right_motor_set_speed(0);
+}
+
+void go_straight(void){
+	left_motor_set_speed(SPEED); //Not set to max because I don't want to break the robot
+	right_motor_set_speed(SPEED); //Also might be more sensitive at higher speed
+}
+
 //Using double because idk how to get arctan in float SHOULD BE CHANGED !!!
 //Using degrees to be able to use ints
 //Angles are in trigonometric direction
-float imu_bearing(int16_t acc_x_calibrated, int16_t acc_y_calibrated){
-	if(acc_y_calibrated <= IMU_EPSILON*g && acc_y_calibrated > 0){
+float imu_bearing(int16_t acc_x, int16_t acc_y){
+	//Limit cases to avoid division by zero
+	if(acc_y <= IMU_EPSILON*2/IMU_RESOLUTION && acc_x > 0){
 		return 90 ;
 	}
-	else if(acc_y_calibrated <= IMU_EPSILON*g && acc_x_calibrated < 0){
+	if(acc_y <= IMU_EPSILON*2/IMU_RESOLUTION && acc_x < 0){
 		return -90 ;
 	}
-	return 57.3f*atan2(acc_x_calibrated,acc_y_calibrated);
+	return 57.3f*atan2(acc_x,acc_y);
 	//57.3 is to convert to degree
 	//f so it's compiled as a float
 }
 
-//Using double to match imu_bearing
-//THIS IS A SIMPLIFIED VERSION FOR TESTING
-float prox_bearing(void){
-	/*bool coll__front_left = get_calibrated_prox(PROX_FRONT_LEFT);
-	bool coll_diag_left = get_calibrated_prox(PROX_DIAG_LEFT);
-	bool coll_front_right = get_calibrated_prox(PROX_FRONT_RIGHT);
-	bool coll_diag_right = get_calibrated_prox(PROX_DIAG_RIGHT);
-	bool coll_right = get_calibrated_prox(PROX_RIGHT);
-	bool coll_left = get_calibrated_prox(PROX_LEFT);*/
-
-	//Redundant to assign memory space unless these are used more than once
-	//Done here to print
-	int prox_left= get_calibrated_prox(PROX_FRONT_LEFT);
-	int prox_right= get_calibrated_prox(PROX_FRONT_RIGHT);
-
-	//Print proximity values
-	//Does .2f work on int ?
-	chprintf((BaseSequentialStream *)&SD3, "Prox_L = %i \r\n", prox_left);
-	chprintf((BaseSequentialStream *)&SD3, "Prox_R = %i \r\n", prox_right);
-
-	if(prox_left >= PROX_THRESHOLD){
-		return -30 ; //In trigonometric direction, corresponds to turning clockwise
+void angle_motor_treatment(float angle){
+	if(angle<0){
+		turn_left();
 	}
-	if(prox_right >= PROX_THRESHOLD){
-		return 30 ;
+	if(angle>0){
+		turn_right();
 	}
-	return 0;
-
-	//Variant: use a static variable
-	//Should prevent flip-flopping, but slower redressement
-	/*
-	static double bearing = 0;
-	if(get_calibrated_prox(PROX_FRONT_LEFT) <= PROX_THRESHOLD and get_calibrated_prox(PROX_FRONT_LEFT) <= PROX_THRESHOLD){
-		bearing += 90 //plus by convention
-		return bearing ;
+	if(angle == 0){
+		go_straight();
 	}
-	else if(get_calibrated_prox(PROX_FRONT_LEFT) <= PROX_THRESHOLD){
-		bearing -= 30 ;
-		return bearing ;
-	}
-	else if(get_calibrated_prox(PROX_FRONT_LEFT) <= PROX_THRESHOLD){
-		bearing += 30;
-		return bearing ;
-	}
-	else{
-		bearing -= (fabs(bearing)/bearing) * 5 ;
-		return bearing;
-	}*/
 }
 
-static THD_WORKING_AREA(waSetPath, 256); //Should not need much memory (no tables)
+static THD_WORKING_AREA(waSetPath, 256);
 
 static THD_FUNCTION(SetPath, arg) {
 
     chRegSetThreadName(__FUNCTION__);
     (void)arg;
 
-    calibrate_acc(); //Collects samples for calibration
-    //Offsets calculated in advance to speed up program
-    int16_t offset_x = get_acc_offset(X_AXIS);
-    int16_t offset_y = get_acc_offset(Y_AXIS);
-    int16_t offset_z = get_acc_offset(Z_AXIS);
+    int16_t offset_x = 0;
+    int16_t offset_y = 0;
+    int16_t offset_z = 0;
 
-    // Functions to use in the thread:
-    //get_calibrated_prox( /*0 to 7*/ )
-
-    //get_acc_offset( /* 0 to 2 = x,y,z */ );
-
-    //get_acc_filtered( /* 0 to 2*/ , /* filter size = nb samples*/ );
-
-    //left_motor_set_speed( /* int < 1100 */ );
-    //right_motor_set_speed();
+    //Protection in case of calibration with the sensors covered
+    do{
+    	calibrate_acc(); //Collects samples for calibration
+    	offset_x = get_acc_offset(X_AXIS);
+    	offset_y = get_acc_offset(Y_AXIS);
+    	offset_z = get_acc_offset(Z_AXIS);
+    }while((offset_x >= 500) || (offset_y >= 500) || (offset_z >= 500));
 
     systime_t time;
-    int16_t acc_x;
-    int16_t acc_y;
-    int16_t acc_z;
-    float angle_imu;
-    float angle_prox;
-    float angle_res;
+
+    int16_t acc_x_calibrated;
+    int16_t acc_y_calibrated;
+    int16_t acc_z_calibrated;
+
+    int prox_front_left;
+    int prox_front_right;
+    int prox_diag_left;
+    int prox_diag_right;
+    //int prox_left;
+    //int prox_right;
+
+    bool coll_front_left;
+    bool coll_front_right;
+    bool coll_diag_left;
+    bool coll_diag_right;
+    //bool coll_left;
+    //bool coll_right;
+
+    float imu_angle = 0;
+    float imu_angle_prev; //used to help treat zero case
+
+    //I don't think these need to be static
+    //MAY BE A SOURCE OF ERRORS
+    bool avoiding_coll_left = 0 ;
+    bool avoiding_coll_right = 0;
+
+    bool stuck_toggle = 0; //Treats the case where it hits an obstacle head-on
+
     while(1){
     	time = chVTGetSystemTime();
 
-    	//It's redundant to assign memory space unless these are called more than once
-    	//Done here so it can be printed
-    	acc_x=(get_acceleration(X_AXIS)-offset_x)*(2*g/IMU_RESOLUTION);
-    	acc_y=(get_acceleration(Y_AXIS)-offset_y)*(2*g/IMU_RESOLUTION);
-    	acc_z=(get_acceleration(Z_AXIS)-offset_z)*(2*g/IMU_RESOLUTION);
+//Collect and print all values
+    	//It's redundant to assign memory space unless variables are called more than once
+    	//Done here because all variables are also printed
+    	//Remove for final version
+
+    	//Collect acceleration values
+    	acc_x_calibrated=(get_acceleration(X_AXIS)-offset_x);
+    	acc_y_calibrated=(get_acceleration(Y_AXIS)-offset_y);
+    	acc_z_calibrated=(get_acceleration(Z_AXIS)-offset_z);
 
     	//Print the offsets:
-    	 chprintf((BaseSequentialStream *)&SD3, "Offset_X = %i \r\n", offset_x);
-    	 chprintf((BaseSequentialStream *)&SD3, "Offset_Y = %i \r\n", offset_y);
-    	 chprintf((BaseSequentialStream *)&SD3, "Offset_Z = %i \r\n", offset_z);
+    	chprintf((BaseSequentialStream *)&SD3, "Offset_X = %d \r\n", offset_x);
+    	chprintf((BaseSequentialStream *)&SD3, "Offset_Y = %d \r\n", offset_y);
+    	chprintf((BaseSequentialStream *)&SD3, "Offset_Z = %d \r\n", offset_z);
 
-    	 //Print the accelerometer values:
-    	 chprintf((BaseSequentialStream *)&SD3, "Acc_X = %i \r\n", acc_x);
-    	 chprintf((BaseSequentialStream *)&SD3, "Acc_Y = %i \r\n", acc_y);
-    	 chprintf((BaseSequentialStream *)&SD3, "Acc_Z = %i \r\n", acc_z);
+    	//Print the accelerometer values:
+    	chprintf((BaseSequentialStream *)&SD3, "Acc_X = %d \r\n", acc_x_calibrated);
+    	chprintf((BaseSequentialStream *)&SD3, "Acc_Y = %d \r\n", acc_y_calibrated);
+    	chprintf((BaseSequentialStream *)&SD3, "Acc_Z = %d \r\n", acc_z_calibrated);
 
-    	/*if(acc_z <= -(1 - IMU_EPSILON)*g){ //Top reached //Minus because z axis points up
-    		left_motor_set_speed(0);
-    		right_motor_set_speed(0);
-    		//MAY WELL BE A SOURCE OF ERRORS
-    		chprintf((BaseSequentialStream *)&SD3, "TOP REACHED");
-    	}*/
-    	//else{ //VERY VERY BAD JUST FOR TESTING
-    		angle_imu = imu_bearing(acc_x, acc_y);
-    		angle_prox = prox_bearing();
-    		angle_res=COEFF_IMU * imu_bearing(acc_x, acc_y) + COEFF_PROX*prox_bearing(); //Penalty optmization problem
+    	//Collect the proximity values
+    	prox_front_left = get_calibrated_prox(PROX_FRONT_LEFT);
+    	prox_front_right = get_calibrated_prox(PROX_FRONT_RIGHT);
+    	prox_diag_left = get_calibrated_prox(PROX_DIAG_LEFT);
+    	prox_diag_right = get_calibrated_prox(PROX_DIAG_RIGHT);
+    	//prox_left = get_calibrated_prox(PROX_LEFT);
+        //prox_right = get_calibrated_prox(PROX_RIGHT);
 
-    		//Print angles values
-    		chprintf((BaseSequentialStream *)&SD3, "Angle_IMU = %.2f \r\n", angle_imu);
-    		chprintf((BaseSequentialStream *)&SD3, "Angle_PROX = %.2f \r\n", angle_prox);
-    		chprintf((BaseSequentialStream *)&SD3, "Angle_RES = %.2f \r\n", angle_res);
+        //Print proximity values
+        chprintf((BaseSequentialStream *)&SD3, "Prox_FL = %d \r\n", prox_front_left);
+        chprintf((BaseSequentialStream *)&SD3, "Prox_FR = %d \r\n", prox_front_right);
+        chprintf((BaseSequentialStream *)&SD3, "Prox_DL = %d \r\n", prox_diag_left);
+        chprintf((BaseSequentialStream *)&SD3, "Prox_DR = %d \r\n", prox_diag_right);
+        //chprintf((BaseSequentialStream *)&SD3, "Prox_L = %d \r\n", prox_left);
+        //chprintf((BaseSequentialStream *)&SD3, "Prox_R = %d \r\n", prox_right);
 
-    		left_motor_set_speed(SPEED-angle_res);
-    		right_motor_set_speed(SPEED+angle_res);
-    	//}
+        //Collect collisions data
+        coll_front_left = prox_front_left >= PROX_THRESHOLD_FRONT; // 1 = collision
+        coll_front_right = prox_front_left >= PROX_THRESHOLD_FRONT;
+        coll_diag_left = prox_front_left >= PROX_THRESHOLD_DIAG;
+        coll_diag_right = prox_front_left >= PROX_THRESHOLD_DIAG;
+        //coll_left = prox_front_left >= PROX_THRESHOLD;
+        //coll_right = prox_front_left >= PROX_THRESHOLD;
+
+       //Print collision values
+        chprintf((BaseSequentialStream *)&SD3, "Coll_FL = %d \r\n", coll_front_left);
+        chprintf((BaseSequentialStream *)&SD3, "Coll_FR = %d \r\n", coll_front_right);
+        chprintf((BaseSequentialStream *)&SD3, "Coll_DL = %d \r\n", coll_diag_left);
+        chprintf((BaseSequentialStream *)&SD3, "Coll_DR = %d \r\n", coll_diag_right);
+        //chprintf((BaseSequentialStream *)&SD3, "Coll_L = %d \r\n", coll_left);
+        //chprintf((BaseSequentialStream *)&SD3, "Coll_R = %d \r\n", coll_right);
+
+        //Maximal pitch angle
+        imu_angle_prev = imu_angle;
+        imu_angle=imu_bearing(acc_x_calibrated, acc_y_calibrated);
+
+        //Print imu angle
+        chprintf((BaseSequentialStream *)&SD3, "IMU_angle = %.4f \r\n", imu_angle);
+
+//Movement algorithm:
+//The robot usually follows the imu angle
+//In case of obstacle, it turns until there is no obstacle straight ahead,
+        //then goes straight until the diagonal sensors are clear
+        //then goes back to normal function
+
+//Particular cases:
+        //If both sensors blocked, follows imu angle direction
+        //
+
+//Giant if-else of all possible cases
+//Has some overlaps in motor settings but not in flag settings
+
+        //Case top reached
+        if(acc_z_calibrated <= -(1 - IMU_EPSILON)*2/IMU_RESOLUTION){ //Minus because z axis points up // 2/res corresponds to g
+           stop_motors();
+           chprintf((BaseSequentialStream *)&SD3, "TOP REACHED");
+        }
+
+        //Case no previous collisions
+        else if(! avoiding_coll_left && !avoiding_coll_right){
+        	if(!coll_front_left && !coll_front_right){ //No current collisions
+        		angle_motor_treatment(imu_angle);
+        	}
+        	else if(coll_front_left){
+        		avoiding_coll_left = 1;
+        		turn_right();
+        	}
+        	else if(coll_front_right){
+        	    avoiding_coll_right = 1;
+        	    turn_left();
+        	}
+        	else if(coll_front_left && coll_front_right){
+        		avoiding_coll_right = 1;
+        		avoiding_coll_left = 1;
+        		if(imu_angle != 0){ //Probably not super elegant
+        			angle_motor_treatment(imu_angle);
+        		}
+        		else{ //treats the case where it hit an obstacle aligned with optimal climb angle
+        			if(stuck_toggle){ //tries to go one way, next it'll try the next
+        				++stuck_toggle;
+        				turn_left();
+        			}
+        			else{
+        				++stuck_toggle;
+        				turn_right();
+        			}
+        		}
+        	}
+        }
+
+        //Case avoiding collision on left
+        else if(avoiding_coll_left){
+        	if(coll_front_left){ //Pretty sure this is redundant
+        		turn_right();
+        	}
+        	else if(coll_front_right){
+        		avoiding_coll_right = 1;
+        		 if(imu_angle != 0){ //Probably not super elegant
+        			 angle_motor_treatment(imu_angle);
+        		 }
+        		 else{ //treats the case where it hit an obstacle aligned with optimal climb angle
+        		     if(stuck_toggle){ //tries to go one way, next it'll try the next
+        		    	 ++stuck_toggle;
+        		    	 turn_left();
+        		     }
+        		     else{
+        		    	 ++stuck_toggle;
+        		    	 turn_right();
+        		     }
+        		 }
+        	}
+        	else if(coll_diag_left){
+        		go_straight();
+        	}
+        	else{
+        		avoiding_coll_left = 0;
+        		angle_motor_treatment(imu_angle);
+        	}
+        }
+
+        //Case avoiding collision on right
+        else if(avoiding_coll_right){
+        	if(coll_front_left){
+        		coll_front_left = 1;
+        		if(imu_angle != 0){ //Probably not super elegant
+        			angle_motor_treatment(imu_angle);
+        		}
+        		else{ //treats the case where it hit an obstacle aligned with optimal climb angle
+        		    if(stuck_toggle){ //tries to go one way, next it'll try the next
+        		    	++stuck_toggle;
+        		    	turn_left();
+        		    }
+        		    else{
+        		    	++stuck_toggle;
+        		    	turn_right();
+        		    }
+        		}
+        	}
+        	else if(coll_front_right){
+        		turn_left(); //Probably redundant
+        	}
+        	else if(coll_diag_right){
+        		go_straight();
+        	}
+        	else{
+        		avoiding_coll_right = 0;
+        		angle_motor_treatment(imu_angle);
+        	}
+        }
+
+        //Case avoiding head-on collision
+        else{
+        	if(coll_front_left || coll_front_left){
+        		//SET UP COUNTER TO OPTIMIZE MOVEMENT, ie
+        		//PREVENT IT FROM SPINNING PAST OPTIMAL PITCH ANGLE
+
+        		//Keep turning
+        		//This block is here so I don't forget this case and in case you actually need to keep updating the motors
+        	}
+        	else if(imu_angle_prev >0 && coll_diag_right){
+        		go_straight();
+        	}
+        	else if(imu_angle_prev < 0 && coll_diag_left){
+        	    go_straight();
+        	}
+        	else if(imu_angle_prev == 0){
+        		if((stuck_toggle == 0 && coll_diag_right) || (stuck_toggle == 1 && coll_diag_left)){ //A diag sensor sees an opening
+        			go_straight();
+        		}
+        	}
+        	else{
+        		avoiding_coll_left = 0;
+        		avoiding_coll_right = 0;
+        		angle_motor_treatment(imu_angle);
+        	}
+        }
 
     	chThdSleepUntilWindowed(time, time + MS2ST(10)); //100 Hz
     }
--- CamReg_correction/climb.h
+++ CamReg_correction/climb.h
@@ -1,6 +1,8 @@
 #ifndef CLIMB_H
 #define CLIMB_H
 
+#define STATIC_PROX
+
 #define PROX_FRONT_RIGHT 			0
 #define PROX_FRONT_LEFT 			7
 #define PROX_DIAG_RIGHT				1
@@ -14,7 +16,8 @@
 
 #define g							9.81
 
-#define SPEED						300 	//Goes up to 1100
+#define SPEED						550 	//Goes -1100 to 1100
+#define SPEED_MAX					1100
 
 //#define IMU_SAMPLE_SIZE				50 		//The base functions use 50
 #define IMU_EPSILON					0.01	//Determined empirically
@@ -22,21 +25,27 @@
 #define IMU_RESOLUTION				32000
 #define IMU_MAX						2*g
 
-#define PROX_THRESHOLD				60 	//Determined empirically //
+#define PROX_THRESHOLD_FRONT		60 	//Determined empirically
+#define PROX_THRESHOLD_DIAG			60 	//Determined empirically //Should be smaller than front
 
 #define COEFF_IMU					0.6		//Determined empirically
 #define COEFF_PROX					0.4		//Determined empirically
 
 
-float imu_bearing(int16_t acc_x_calibrated, int16_t acc_y_calibrated);
-float prox_bearing(void);
+void turn_left(void);
 
-/*Thread to:
- *	- Read IMU values
- *	- Read prox values
- *	- Calculate a path
- *	- Set the motors to that path
- */
+void turn_right(void);
+
+void stop_motors(void);
+
+void go_straight(void);
+
+
+float imu_bearing(int16_t acc_x, int16_t acc_y);
+
+void angle_motor_treatment(float angle);
+
+
 void set_path_start(void);
 
 #endif /* CLIMB_H */
