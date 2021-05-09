1382662efba48455304de46ef627dc5751c28946
Matteo Righi <matteo.righibassu@gmail.com>
2021-05-09 16:38:45 +0200

    Added leds files

---
+++ CamReg_correction/leds_handler.h
@@ -0,0 +1,8 @@
+#ifndef LEDS_HANDLER_H_
+#define LEDS_HANDLER_H_
+
+void leds_handler(int state, int16_t bearing);
+
+void blink_leds(int state, int16_t bearing, int blink_period_cycles);
+
+#endif /* LEDS_HANDLER_H_ */
