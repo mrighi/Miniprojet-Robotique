db22b0c1a3888e8c3328c0be95bb0f267db96461
Matteo Righi <matteo.righibassu@gmail.com>
2021-05-11 10:48:19 +0200

    Removed files

--- climby/leds_handler.h
+++
@@ -1,8 +0,0 @@
-#ifndef LEDS_HANDLER_H_
-#define LEDS_HANDLER_H_
-
-void leds_handler(int state, int16_t bearing);
-
-void blink_leds(int state, int16_t bearing, int blink_period_cycles);
-
-#endif /* LEDS_HANDLER_H_ */
